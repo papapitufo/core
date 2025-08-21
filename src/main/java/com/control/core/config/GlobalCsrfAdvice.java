770e6e1d50b8de0d82215cb04944207b14437f2a
robi moller <robimoller@gmail.com>
2025-08-20 21:57:08 -0400

    version 1.0.28

---
+++ src/main/java/com/control/core/config/GlobalCsrfAdvice.java
@@ -0,0 +1,15 @@
+package com.control.core.config;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalCsrfAdvice {
+    @ModelAttribute("_csrf")
+    public CsrfToken csrfToken(HttpServletRequest request) {
+        return (CsrfToken) request.getAttribute("_csrf");
+    }
+}
